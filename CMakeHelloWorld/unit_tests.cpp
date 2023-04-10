3ea5b728dc402013744be55c6e03c4a97c335667
Tyler Holmes <tymaho@aol.com>
2023-04-10 18:17:43 -0400

    Fully over documented CMakeHelloWorld

--- CMakeHelloWorld/unit_tests.cpp
+++ CMakeHelloWorld/unit_tests.cpp
@@ -1,6 +1,20 @@
-#include <HelloWorld.h>
-#include <assert.h>
+/************************************************************************
+ * CMake Hello World                                                    
+ * @file unit_tests.cpp                                                 
+ * @brief Unit test that checks if program truly prints "Hello World!"  
+ * @author Tyler Holmes                                                 
+ * Contact: tmh6859@gmail.com                                           
+ * @date April 10, 2023                                                 
+*************************************************************************/
 
+#include <assert.h>
+#include "HelloWorld.h"
+
+/*******************************************************************************
+ * Tests if HelloWorld library prints "Hello World!" correctly 
+ * 
+ * @return 0 on successful run
+ *******************************************************************************/
 int main() 
 {
   assert("Hello world!" == helloWorld());
