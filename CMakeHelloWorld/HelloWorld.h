648964cb8b648d86ea6916c2aeae969318590b54
Tyler Holmes <tymaho@aol.com>
2023-04-10 18:18:21 -0400

    Unstaged files related to last commit

--- CMakeHelloWorld/HelloWorld.h
+++ CMakeHelloWorld/HelloWorld.h
@@ -1,5 +1,18 @@
+/********************************************************************************
+ * CMake Hello World                                                            
+ * @file HelloWorld.h                                                           
+ * @brief Function declaration for function that returns "Hello World!" string  
+ * @author Tyler Holmes                                                         
+ * Contact: tmh6859@gmail.com                                                   
+ * @date April 10, 2023                                                         
+*********************************************************************************/
+
 #ifndef HELLOWORLD_H
 #define HELLOWORLD_H
+
 #include <string>
+
+/// Returns "Hello World!" as a string
 std::string helloWorld();
-#endif // SQR_H
+
+#endif // HELLOWORLD_H
--- CMakeHelloWorld/main.cpp
+++ CMakeHelloWorld/main.cpp
@@ -1,6 +1,20 @@
-#include "HelloWorld.h"
-#include <iostream>
+/********************************************
+ * CMake Hello World    
+ * @file main.cpp
+ * @brief Prints the classic "Hello World!"
+ * @author Tyler Holmes
+ * Contact: tmh6859@gmail.com
+ * @date April 10, 2023
+********************************************/
 
+#include <iostream>
+#include "HelloWorld.h"
+
+/*******************************************************************************
+ * Prints "Hello World!" 
+ * 
+ * @return 0 on successful run
+ *******************************************************************************/
 int main ()
 {
     std::cout << helloWorld() << std::endl;
